5c034873e67bedc0a313effd7938616fb9314dc2
amorimlipeh <am.felipe1994@gmail.com>
2026-03-11 23:35:15 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -78,6 +78,13 @@ const buildWmsMap = (produtos) => {
 };
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(uploadsDir));
